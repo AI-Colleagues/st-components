9b6e50637da9fd12be7d783eb422790157254bf3
Shaojie Jiang <shaojiejiang@MacBook.local>
2024-03-31 12:37:42 +0200

    Import bug fixed.

--- image_chat.py
+++ image_chat.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # from file_message import my_component
-from chat_input import my_component
+from file_message import my_component
 
 # Use it:
 value = my_component(name="Hello World")
