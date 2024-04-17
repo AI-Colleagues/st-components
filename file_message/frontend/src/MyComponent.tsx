8930b9d8893e020b23ece3774184e718b1fcb232
Shaojie Jiang <shaojiejiang@MacBook.local>
2024-04-17 10:58:39 +0200

    Progressing.

--- file_message/frontend/src/MyComponent.tsx
+++ file_message/frontend/src/MyComponent.tsx
@@ -1,4 +1,4 @@
-import './message.css'
+import "./message.css"
 import {
   Streamlit,
   StreamlitComponentBase,
@@ -7,144 +7,177 @@ import {
 import React, { ReactNode } from "react"
 
 import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  ChannelList,
+  MessageList,
+  Thread,
+  Window,
+  MessageInput,
   useMessageInputContext,
   useComponentContext,
-  AttachmentPreviewList as DefaultAttachmentPreviewList,
-  LinkPreviewList as DefaultLinkPreviewList,
-} from 'stream-chat-react';
-
-const AttachmentUploadButton = () => {
-  const { uploadNewFiles, isUploadEnabled, maxFilesLeft } = useMessageInputContext();
-
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    const files = e.currentTarget.files;
-
-    if (files && files.length > 0) {
-      uploadNewFiles(files);
-    }
-  }
-
-  if (maxFilesLeft === 0) {
-    return null;
-  }
-
-  return (
-    <label className='message-input__button'>
-      <input type='file' className='visually-hidden' onChange={handleChange} />
-      📎
-    </label>
-  );
-};
-
-
+  ImageUpload,
+  FileUpload,
+  DefaultStreamChatGenerics,
+} from "stream-chat-react"
 
 const CustomAttachmentPreviewList = () => {
-  const { fileOrder = [], imageOrder = [] } = useMessageInputContext();
+  const { fileOrder = [], imageOrder = [] } = useMessageInputContext()
 
   if (fileOrder.length === 0 && imageOrder.length === 0) {
-    return null;
+    return null
   }
 
   return (
-    <ul className='message-input__attachments'>
-      {imageOrder.map((id) => (
-        <li className='message-input__attachment' key={id}>
+    <ul className="message-input__attachments">
+      {imageOrder.map((id: string) => (
+        <li className="message-input__attachment" key={id}>
           <ImageAttachmentPreview id={id} />
         </li>
       ))}
-      {fileOrder.map((id) => (
+      {fileOrder.map((id: string) => (
         <li key={id}>
           <FileAttachmentPreview id={id} />
         </li>
       ))}
     </ul>
-  );
-};
+  )
+}
 
 const ImageAttachmentPreview = ({ id }: { id: string }) => {
-  const { imageUploads } = useMessageInputContext();
-  const image = imageUploads[id];
-  const url = image.previewUri ?? image.url ?? 'fallback.webm';
+  const { imageUploads } = useMessageInputContext()
+  const image = imageUploads[id]
+  const url = image.previewUri ?? image.url ?? "fallback.webm"
 
   return (
     <div
-      className='message-input__attachment-preview message-input__attachment-preview_image'
+      className="message-input__attachment-preview message-input__attachment-preview_image"
       style={{ backgroundImage: `url(${url})` }}
       aria-label={image.file.name}
     >
-      <AttachmentActions attachment={image} type='image' />
+      <AttachmentActions attachment={image} type="image" />
     </div>
-  );
-};
+  )
+}
 
 const FileAttachmentPreview = ({ id }: { id: string }) => {
-  const { fileUploads } = useMessageInputContext();
-  const attachment = fileUploads[id];
+  const { fileUploads } = useMessageInputContext()
+  const attachment = fileUploads[id]
 
   return (
-    <div className='message-input__attachment-preview message-input__attachment-preview_file'>
+    <div className="message-input__attachment-preview message-input__attachment-preview_file">
       📄 {attachment.file.name} <br />({attachment.file.size} bytes)
-      <AttachmentActions attachment={attachment} type='file' />
+      <AttachmentActions attachment={attachment} type="file" />
     </div>
-  );
-};
+  )
+}
+type AttachmentType = "image" | "file"
 
-const AttachmentActions = ({ type, attachment }) => {
-  const { removeImage, uploadImage, removeFile, uploadFile } = useMessageInputContext();
-  let [remove, upload] = type === 'image' ? [removeImage, uploadImage] : [removeFile, uploadFile];
-  let children = null;
+type CustomAttachment = ImageUpload<DefaultStreamChatGenerics> | FileUpload
 
-  if (attachment.state === 'uploading') {
-    children = <div className='message-input__attachment-action'>Loading...</div>;
+const AttachmentActions = ({
+  type,
+  attachment,
+}: {
+  type: AttachmentType
+  attachment: CustomAttachment
+}) => {
+  const { removeImage, uploadImage, removeFile, uploadFile } =
+    useMessageInputContext()
+  let [remove, upload] =
+    type === "image" ? [removeImage, uploadImage] : [removeFile, uploadFile]
+  let children = null
+
+  if (attachment.state === "uploading") {
+    children = (
+      <div className="message-input__attachment-action">Loading...</div>
+    )
   }
 
-  if (attachment.state === 'finished') {
+  if (attachment.state === "finished") {
     children = (
       <>
         <a
-          className='message-input__attachment-action'
+          className="message-input__attachment-action"
           href={attachment.url}
-          target='_blank'
-          rel='noreferrer'
+          target="_blank"
+          rel="noreferrer"
         >
           📥
         </a>
-        <button className='message-input__attachment-action' onClick={() => remove(attachment.id)}>
+        <button
+          className="message-input__attachment-action"
+          onClick={() => remove(attachment.id)}
+        >
           ❌
         </button>
       </>
-    );
+    )
   }
 
-  if (attachment.state === 'failed') {
-    <button className='message-input__attachment-action' onClick={() => upload(attachment.id)}>
+  if (attachment.state === "failed") {
+    ;<button
+      className="message-input__attachment-action"
+      onClick={() => upload(attachment.id)}
+    >
       Failed. Retry?
-    </button>;
+    </button>
   }
 
-  return <div className='message-input__attachment-actions'>{children}</div>;
-};
+  return <div className="message-input__attachment-actions">{children}</div>
+}
 
+const AttachmentUploadButton = () => {
+  const { uploadNewFiles, isUploadEnabled, maxFilesLeft } =
+    useMessageInputContext()
 
-const CustomMessageInput = () => {
-  const { text, handleChange, handleSubmit } = useMessageInputContext();
-  const { AttachmentPreviewList = DefaultAttachmentPreviewList } = useComponentContext();
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const files = e.currentTarget.files
+
+    if (files && files.length > 0) {
+      uploadNewFiles(files)
+    }
+  }
+
+  if (maxFilesLeft === 0) {
+    return null
+  }
 
   return (
-    <div className='message-input'>
-      <div className='message-input__composer'>
+    <label className="message-input__button">
+      <input type="file" className="visually-hidden" onChange={handleChange} />
+      📎
+    </label>
+  )
+}
+
+const CustomMessageInput = () => {
+  const { text, handleChange, handleSubmit } = useMessageInputContext()
+  const { AttachmentPreviewList = CustomAttachmentPreviewList } =
+    useComponentContext()
+
+  return (
+    <div className="message-input">
+      <div className="message-input__composer">
         <AttachmentUploadButton />
-        <textarea className='message-input__input' value={text} onChange={handleChange} />
-        <button type='button' className='message-input__button' onClick={handleSubmit}>
+        <textarea
+          className="message-input__input"
+          value={text}
+          onChange={handleChange}
+        />
+        <button
+          type="button"
+          className="message-input__button"
+          onClick={handleSubmit}
+        >
           ⬆️
         </button>
       </div>
-      <CustomAttachmentPreviewList />
+      <AttachmentPreviewList />
     </div>
-  );
-};
-
-
+  )
+}
 
 interface State {
   isFocused: boolean
@@ -173,8 +206,9 @@ class MyComponent extends StreamlitComponentBase<State> {
     if (theme) {
       // Use the theme object to style our button border. Alternatively, the
       // theme style is defined in CSS vars.
-      const borderStyling = `1px solid ${this.state.isFocused ? theme.primaryColor : "gray"
-        }`
+      const borderStyling = `1px solid ${
+        this.state.isFocused ? theme.primaryColor : "gray"
+      }`
       style.border = borderStyling
       style.outline = borderStyling
     }
@@ -182,11 +216,18 @@ class MyComponent extends StreamlitComponentBase<State> {
     // Show a chat input component. There is a message box, a send button, and a attachment button for attaching images and other files.
     return (
       <div>
-        <CustomMessageInput/>
+        <CustomMessageInput />
+        {/* <Chat> */}
+        {/* <Channel>
+          <Window> */}
+        {/* <MessageInput Input={CustomMessageInput} /> */}
+        {/* </Window>
+          <Thread />
+        </Channel> */}
+        {/* </Chat> */}
       </div>
-    );
+    )
   }
-
 }
 
 // "withStreamlitConnection" is a wrapper function. It bootstraps the
